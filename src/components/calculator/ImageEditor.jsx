ee250500ae8b46700aa2232242dab4efed1b711b
James Croft <jamz_c@hotmail.co.uk>
2024-12-05 07:22:19 +0000

    Add preset selection options to ImageEditor

--- src/components/calculator/ImageEditor.jsx
+++ src/components/calculator/ImageEditor.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { TextField, IconButton, Grid2 as Grid, Button } from "@mui/material";
+import {
+  TextField,
+  IconButton,
+  Grid2 as Grid,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  ListSubheader,
+} from "@mui/material";
 import { Add, Delete, FileCopy } from "@mui/icons-material";
 import { useBoundStore } from "../../stores";
 
@@ -8,30 +18,113 @@ const ImageEditor = () => {
   const addImage = useBoundStore((state) => state.addImage);
   const updateImage = useBoundStore((state) => state.updateImage);
   const removeImage = useBoundStore((state) => state.removeImage);
+  const presets = useBoundStore((state) => state.presets);
+  const runCalculation = useBoundStore((state) => state.runCalculation);
 
-  const addNewImage = () => {
-    addImage({ height: 0, width: 0, multiplier: 1 });
+  const selectPreset = (preset, index) => {
+    updateImage(index, "preset", preset);
+
+    const selectedPreset = presets
+      .flatMap((presetGroup) =>
+        presetGroup.items.find((p) => p.name === preset)
+      )
+      .filter(Boolean)[0];
+
+    if (selectedPreset) {
+      updateImage(index, "height", selectedPreset.height);
+      updateImage(index, "width", selectedPreset.width);
+    }
+
+    runCalculation();
   };
 
-  const cloneImage = (index) => {
+  const addNewImage = () => {
+    addImage({ height: 0, width: 0, multiplier: 1, preset: "Custom" });
+
+    runCalculation();
+  };
+
+  const updateExistingImage = (index, field, value) => {
+    updateImage(index, field, value);
+
+    const image = images[index];
+
+    const selectedPreset = presets
+      .flatMap((presetGroup) =>
+        presetGroup.items.find(
+          (p) => p.height == image.height && p.width == image.width
+        )
+      )
+      .filter(Boolean)[0];
+
+    if (selectedPreset) {
+      updateImage(index, "preset", selectedPreset.name);
+    } else {
+      updateImage(index, "preset", "Custom");
+    }
+
+    runCalculation();
+  };
+
+  const cloneExistingImage = (index) => {
     const image = images[index];
     addImage({
       height: image.height,
       width: image.width,
       multiplier: image.multiplier,
+      preset: image.preset,
     });
+
+    runCalculation();
+  };
+
+  const removeExistingImage = (index) => {
+    removeImage(index);
+
+    runCalculation();
   };
 
   return (
     <>
       {images.map((image, index) => (
         <Grid container spacing={1} alignItems="center" key={index}>
+          <FormControl fullWidth margin="normal">
+            <InputLabel id={`image-preset-${index}`}>Preset</InputLabel>
+            <Select
+              labelId={`image-preset-${index}`}
+              value={image.preset}
+              onChange={(e) => selectPreset(e.target.value, index)}
+              label="Preset"
+            >
+              <MenuItem value="Custom">
+                <em>Custom</em>
+              </MenuItem>
+              {presets.flatMap((presetGroup) => [
+                <ListSubheader key={`subheader-${presetGroup.name}`}>
+                  {presetGroup.name}
+                </ListSubheader>,
+                ...presetGroup.items.map((preset) => (
+                  <MenuItem key={preset.name} value={preset.name}>
+                    <Grid container spacing={1} alignItems="center">
+                      {preset.name}
+                      <div style={{ fontSize: "0.8em", fontStyle: "italic" }}>
+                        ({preset.height} x {preset.width})
+                      </div>
+                    </Grid>
+                  </MenuItem>
+                )),
+              ])}
+            </Select>
+          </FormControl>
+
           <Grid size={{ sm: 4 }}>
             <TextField
               label="Image Height (px)"
               type="number"
               value={image.height}
-              onChange={(e) => updateImage(index, "height", e.target.value)}
+              onChange={(e) =>
+                updateExistingImage(index, "height", e.target.value)
+              }
               margin="normal"
               required
               fullWidth
@@ -42,7 +135,9 @@ const ImageEditor = () => {
               label="Image Width (px)"
               type="number"
               value={image.width}
-              onChange={(e) => updateImage(index, "width", e.target.value)}
+              onChange={(e) =>
+                updateExistingImage(index, "width", e.target.value)
+              }
               margin="normal"
               required
               fullWidth
@@ -53,19 +148,27 @@ const ImageEditor = () => {
               label="Count"
               type="number"
               value={image.multiplier}
-              onChange={(e) => updateImage(index, "multiplier", e.target.value)}
+              onChange={(e) =>
+                updateExistingImage(index, "multiplier", e.target.value)
+              }
               margin="normal"
               required
               fullWidth
             />
           </Grid>
           <Grid size={{ sm: 1 }}>
-            <IconButton onClick={() => cloneImage(index)} color="primary">
+            <IconButton
+              onClick={() => cloneExistingImage(index)}
+              color="primary"
+            >
               <FileCopy />
             </IconButton>
           </Grid>
           <Grid size={{ sm: 1 }}>
-            <IconButton onClick={() => removeImage(index)} color="secondary">
+            <IconButton
+              onClick={() => removeExistingImage(index)}
+              color="secondary"
+            >
               <Delete />
             </IconButton>
           </Grid>
